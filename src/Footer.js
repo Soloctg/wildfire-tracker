d9988a3412869e16193f3217b1866dc0c140b600
solomon-ctg <solomon.iroegbu@gmail.com>
2025-01-14 17:42:14 +0100

    feat: Add Footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by USER on 1/14/2025.
+ */
+
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<h3>Credit  (powered by Solomon .I)</h3>
+		</div>
+	);
+}
+
+export default Footer
